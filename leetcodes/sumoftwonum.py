a491874c1393c95d030c6a9ab934bafc7b443e73
GarField Fang <62200753+Fanstering@users.noreply.github.com>
2021-08-17 15:32:02 +0800

    修改了一些注释
    如题

--- leetcodes/sumoftwonum.py
+++ leetcodes/sumoftwonum.py
@@ -8,20 +8,20 @@ class sum_of_two_num(object):
         #  用快速排序的思想 每次进行加和比较 可以只进行一重循环 不过需要保证整数序列有序
         a = list(nums)
         a.sort()
-        print(a)
+        print(a)  # 使用临时变量对输入的数据进行操作
         i=0
-        j=len(nums)-1
-        result = []
+        j=len(nums)-1  #添加的左右（头尾）指针
+        result = []  
         for k in range(len(nums)):
             print("step ", (k + 1), "left= ", a[i], "right= ", a[j])
             if a[i] + a[j] == target:
                 result= [a[i],a[j]]
                 break
-            if a[i] + a[j] > target: # 一前一后之和大于目标值 右浮标左移
+            if a[i] + a[j] > target: # 一前一后之和大于目标值 尾指针左移
                 j -= 1
-            elif a[i] + a[j] < target:
+            elif a[i] + a[j] < target: # 小于则头指针右移
                 i += 1
-            if j == i: # 有一边移到头了  说明没找到
+            if j == i: # 两个指针相遇  说明没找到
                result = None
                break
         if result == None:
@@ -45,6 +45,7 @@ class sum_of_two_num_cycle(object):
             for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
+ #以下是测试用例
 a = [-1,-2,-3,-4,-5]
 b = sum_of_two_num()
 result = b.twoSum(a,-8)
